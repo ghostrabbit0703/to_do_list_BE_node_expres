f57b57fb520c382177b3df8483b7afdfec289548
ghostrabbir0703 <rummypa123@gmail.com>
2026-09-09 01:51:35 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/ping', (req, res) => {
+    res.json({
+        success: true,
+        message: 'pong'
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        error: 'Not Found',
+        message: `La ruta ${req.method} ${req.originalUrl} no existe`
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(500).json({
+        success: false,
+        error: 'Internal Server Error',
+        message: 'Algo salió mal en el servidor'
+    });
+});
+
+export default app;
